033db4c68d316e8ac5533aa4c5ce42f1d6c01afa
jeepzp <1429361896@qq.com>
2017-08-25 21:45:41 +0800

    Site updated: 2017-08-25 21:45:40

--- js/src/schemes/pisces.js
+++ js/src/schemes/pisces.js
@@ -1,3 +1,3 @@
-// build time:Fri Aug 25 2017 21:37:14 GMT+0800 (中国标准时间)
+// build time:Fri Aug 25 2017 21:45:20 GMT+0800 (中国标准时间)
 $(document).ready(function(){var n=$(".header-inner");var r=$("#sidebar");var e=function(){return n.height()+CONFIG.sidebar.offset};var i=function(n){return r.css({"margin-top":n})};var a=window.matchMedia("(min-width: 991px)");i(e()).show();a.addListener(function(n){if(n.matches){i(e())}})});
 //rebuild by neat 
